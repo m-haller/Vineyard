42d2c5dad563cfdf9c71f389176bf0988977ae3d
Michael Haller <michael.haller@myartwork-gmbh.com>
2018-10-28 16:53:29 +0100

    initial

---
+++ Theme/src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace Theme\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {}
+}
+
