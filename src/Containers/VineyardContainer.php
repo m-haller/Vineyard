c346b8712336437ad2198f206ef4f2bb68804e0c
Michael Haller <michael.haller@myartwork-gmbh.com>
2018-11-03 08:44:01 +0100

    author michael

--- src/Containers/VineyardContainer.php
+++ src/Containers/VineyardContainer.php
@@ -8,6 +8,7 @@ class VineyardContainer
 
     public function call(Twig $twig):string
     {
+        return '<meta name="author" content="michael">';
         return $twig->render('Vineyard::content.Vineyard');
     }
 }
