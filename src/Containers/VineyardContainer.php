7839ab207cf8fdfb65dcd4dabba7945b9a05b344
Michael Haller <michael.haller@myartwork-gmbh.com>
2018-11-09 00:08:09 +0100

    return style

--- src/Containers/VineyardContainer.php
+++ src/Containers/VineyardContainer.php
@@ -8,12 +8,8 @@ class VineyardContainer
 
     public function call(Twig $twig): string
     {
-        // return '<style>
-        // body {background-color: powderblue;}
-        // h1 {color: blue;}
-        // p {color: red;}
-        // </style>';
-        // return '<meta name="author" content="michael">';
-        return $twig->render('Vineyard::Containers.content.Vineyard');
+        return '<style>body {background-color: powderblue;}h1 {color: blue;}p {color: red;}</style>';
+        //return '<meta name="author" content="michael">';
+        //return $twig->render('Vineyard::content.Vineyard');
     }
 }
